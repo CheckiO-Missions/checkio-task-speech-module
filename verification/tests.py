6f382ed2f900d552bddedcfc7bd1491a414fc692
Valentin Bryukhanov <bvv.mag@gmail.com>
2013-09-15 15:24:21 +1200

    referee

--- verification/tests.py
+++ verification/tests.py
@@ -7,30 +7,297 @@ Each test is dict with
     "explanation" -- not necessary key, it's using for additional info in animation.
 """
 
-
 TESTS = {
     "Basics": [
         {
-            "input": [3, 2],
-            "answer": 5,
-            "explanation": "3+2=?"
+            "input": 1,
+            "answer": "one"
         },
         {
-            "input": [5, 7],
-            "answer": 12,
-            "explanation": "5+7=?"
+            "input": 2,
+            "answer": "two"
+        },
+        {
+            "input": 3,
+            "answer": "three"
+        },
+        {
+            "input": 4,
+            "answer": "four"
+        },
+        {
+            "input": 5,
+            "answer": "five"
+        },
+        {
+            "input": 6,
+            "answer": "six"
+        },
+        {
+            "input": 7,
+            "answer": "seven"
+        },
+        {
+            "input": 8,
+            "answer": "eight"
+        },
+        {
+            "input": 9,
+            "answer": "nine"
+        },
+        {
+            "input": 10,
+            "answer": "ten"
+        },
+        {
+            "input": 11,
+            "answer": "eleven"
+        },
+        {
+            "input": 12,
+            "answer": "twelve"
+        },
+        {
+            "input": 13,
+            "answer": "thirteen"
+        },
+        {
+            "input": 14,
+            "answer": "fourteen"
+        },
+        {
+            "input": 15,
+            "answer": "fifteen"
+        },
+        {
+            "input": 16,
+            "answer": "sixteen"
+        },
+        {
+            "input": 17,
+            "answer": "seventeen"
+        },
+        {
+            "input": 18,
+            "answer": "eighteen"
+        },
+        {
+            "input": 19,
+            "answer": "nineteen"
+        },
+        {
+            "input": 999,
+            "answer": "nine hundred ninety nine"
+        },
+        {
+            "input": 784,
+            "answer": "seven hundred eighty four"
+        },
+        {
+            "input": 777,
+            "answer": "seven hundred seventy seven"
+        },
+        {
+            "input": 88,
+            "answer": "eighty eight"
+        },
+        {
+            "input": 44,
+            "answer": "forty four"
+        },
+        {
+            "input": 20,
+            "answer": "twenty"
+        },
+        {
+            "input": 30,
+            "answer": "thirty"
+        },
+        {
+            "input": 40,
+            "answer": "forty"
+        },
+        {
+            "input": 50,
+            "answer": "fifty"
+        },
+        {
+            "input": 60,
+            "answer": "sixty"
+        },
+        {
+            "input": 70,
+            "answer": "seventy"
+        },
+        {
+            "input": 80,
+            "answer": "eighty"
+        },
+        {
+            "input": 90,
+            "answer": "ninety"
+        },
+        {
+            "input": 100,
+            "answer": "one hundred"
+        },
+        {
+            "input": 200,
+            "answer": "two hundred"
+        },
+        {
+            "input": 300,
+            "answer": "three hundred"
+        },
+        {
+            "input": 400,
+            "answer": "four hundred"
+        },
+        {
+            "input": 500,
+            "answer": "five hundred"
+        },
+        {
+            "input": 600,
+            "answer": "six hundred"
+        },
+        {
+            "input": 700,
+            "answer": "seven hundred"
+        },
+        {
+            "input": 800,
+            "answer": "eight hundred"
+        },
+        {
+            "input": 900,
+            "answer": "nine hundred"
+        },
+        {
+            "input": 21,
+            "answer": "twenty one"
+        },
+        {
+            "input": 59,
+            "answer": "fifty nine"
+        },
+        {
+            "input": 70,
+            "answer": "seventy"
+        },
+        {
+            "input": 41,
+            "answer": "forty one"
+        },
+        {
+            "input": 82,
+            "answer": "eighty two"
+        },
+        {
+            "input": 541,
+            "answer": "five hundred forty one"
+        },
+        {
+            "input": 351,
+            "answer": "three hundred fifty one"
+        },
+        {
+            "input": 183,
+            "answer": "one hundred eighty three"
+        },
+        {
+            "input": 516,
+            "answer": "five hundred sixteen"
+        },
+        {
+            "input": 226,
+            "answer": "two hundred twenty six"
+        },
+        {
+            "input": 312,
+            "answer": "three hundred twelve"
+        },
+        {
+            "input": 302,
+            "answer": "three hundred two"
+        },
+        {
+            "input": 509,
+            "answer": "five hundred nine"
+        },
+        {
+            "input": 783,
+            "answer": "seven hundred eighty three"
+        },
+        {
+            "input": 753,
+            "answer": "seven hundred fifty three"
+        },
+        {
+            "input": 940,
+            "answer": "nine hundred forty"
         }
     ],
     "Extra": [
         {
-            "input": [6, 3],
-            "answer": 9,
-            "explanation": "6+3=?"
+            "input": 98,
+            "answer": "ninety eight"
         },
         {
-            "input": [6, 7],
-            "answer": 13,
-            "explanation": "6+7=?"
+            "input": 55,
+            "answer": "fifty five"
+        },
+        {
+            "input": 28,
+            "answer": "twenty eight"
+        },
+        {
+            "input": 23,
+            "answer": "twenty three"
+        },
+        {
+            "input": 100,
+            "answer": "one hundred"
+        },
+        {
+            "input": 761,
+            "answer": "seven hundred sixty one"
+        },
+        {
+            "input": 637,
+            "answer": "six hundred thirty seven"
+        },
+        {
+            "input": 856,
+            "answer": "eight hundred fifty six"
+        },
+        {
+            "input": 787,
+            "answer": "seven hundred eighty seven"
+        },
+        {
+            "input": 466,
+            "answer": "four hundred sixty six"
+        },
+        {
+            "input": 345,
+            "answer": "three hundred forty five"
+        },
+        {
+            "input": 414,
+            "answer": "four hundred fourteen"
+        },
+        {
+            "input": 742,
+            "answer": "seven hundred forty two"
+        },
+        {
+            "input": 592,
+            "answer": "five hundred ninety two"
+        },
+        {
+            "input": 269,
+            "answer": "two hundred sixty nine"
         }
     ]
 }
