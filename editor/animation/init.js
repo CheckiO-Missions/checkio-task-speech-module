f430177b303ebe42246d72427b0c27ebeac70cc5
Oleksandr Zozulia <freemanlex@gmail.com>
2022-11-01 10:36:04 +0200

    next api

--- editor/animation/init.js
+++ editor/animation/init.js
@@ -1,35 +1,6 @@
-requirejs(['ext_editor_io', 'jquery_190', 'raphael_210'],
-    function (extIO, $, TableComponent) {
-        var $tryit;
-        var io = new extIO({
-            functions: {
-                js: 'speechModule',
-                python: 'checkio'
-            },
-            retConsole: function (ret) {
-                $tryit.find(".checkio-result").html("Your result:<br>" + ret);
-            },
-            tryit:function (this_e) {
-                $tryit = $(this_e.extSetHtmlTryIt(this_e.getTemplate('tryit')));
-                $tryit.find('.input-text').focus();
-                const isNumber = function(n) {
-                    return !isNaN(parseFloat(n)) && isFinite(n);
-                }
-                $tryit.find('.bn-check').click(function(e){
-                    var number_text = $tryit.find('.input-text').val();
-                    var number = parseInt(number_text);
-                    if (isNumber(number)){
-                        this_e.extSendToConsoleCheckiO(number);
-                        e.stopPropagation();
-                    }
-                    else {
-                        $tryit.find(".checkio-result").html('Please enter a number.');
-
-                    }
-                    return false;
-                });
-            }
-        });
+requirejs(['ext_editor_io2', 'jquery_190'],
+    function (extIO, $) {
+        var io = new extIO({});
         io.start();
     }
 );
